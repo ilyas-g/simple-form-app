e35e88b636b136094bcf609d235eef6ac4cd0462
Roddoark <ilyas150691@hotmail.fr>
2022-07-21 14:56:41 +0200

    Suppression des fichiers non nécessaire et mise en place d'une première version du formulaire

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [showForm, setShowForm] = useState(false);
+
+  const handleNameChange = event => {
+    const value = event.target.value;
+    setName(value);
+  };
+
+  const handleEmailChange = event => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+
+  const handlePasswordChange = event => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("okoko");
+    console.log(event);
+    console.log(event.target.name.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Create account</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="field">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Jean Dupont" />
+        </div>
+        <div className="field">
+          <label htmlFor="mail">Email</label>
+          <input
+            type="text"
+            name="mail"
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="jeandupont@lereacteur.io" />
+        </div>
+        <div className="field">
+          <label htmlFor="pass">Password</label>
+          <input
+            type="password"
+            name="pass"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="lErEaCtEuR2020" />
+        </div>
+        <div className="field">
+          <label htmlFor="confirm">Confirm your Password</label>
+          <input type="password" name="confirm" placeholder="lErEaCtEuR2020" />
+        </div>
+        <input onClick={() => {
+          setShowForm(true);
+        }} type="submit" value="Register" />
+      </form>
+      <div>
+
+        {showForm && (
+          <p>
+            {name} {email} {password}
+          </p>
+        )}
+      </div>
+    </div >
   );
 }
 
